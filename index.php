0af68a8074370cad06111aa1e81c651c61a644e3
AsuKuma <gabriel.vicente.59@gmail.com>
2017-04-19 13:29:03 +0200

    Début de la v2 de broc'vintage

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>BrocVintage</title>
+    <link rel="stylesheet" href="inc/css/bootstrap.css">
+    <link rel="stylesheet" href="inc/css/style.css">
+    <script src="<?= URL ?>/inc/js/bootstrap.js"></script>
+</head>
