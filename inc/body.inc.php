29e98d76259eda5fb7be817c8b246dd8a474acc2
AsuKuma <gabriel.vicente.59@gmail.com>
2017-06-07 01:06:31 +0200

    optimisation du body

--- inc/body.inc.php
+++ inc/body.inc.php
@@ -11,125 +11,80 @@
         </div>
     </div>';
 
-                if(isset($_GET['page']))
-                {
-                    if($_GET['page'] == 'registration')
-                    {
+    echo '
+    <div class="container">
+        <div class="row">';
+
+                if(isset($_GET['page'])){
+                    if($_GET['page'] == 'registration'){
                         include('registration.php');
-                    }elseif($_GET['page'] == 'connection')
-                    {
+                    }elseif($_GET['page'] == 'connection'){
                         include('connection.php');
-                    }elseif($_GET['page'] == 'product')
-                    {
+                    }elseif($_GET['page'] == 'product'){
+                        include('content.php');
                         echo '
-                        <div class="container">
-    
-                            <div class="row">';
-
-                                include('content.php');
-                                echo '
-                                <div class="col-md-9">';
-                                    include('product.php');
-                                echo '
-                                </div>
-                                <!-- /.col-md-9 -->
-                            </div>
-                            <!-- /.row -->
-                        </div>
-                        <!-- /.container -->';
-                    }elseif($_GET['page'] == 'article')
-                    {
+                        <div class="col-md-9">';
+                            include('product.php');
                         echo '
-                        <div class="container">
-    
-                            <div class="row">';
-
-                                include('content.php');
-                                echo '
-                                <div class="col-md-9">';
-                                    include('article.php');
-                                echo '
-                                </div>
-                                <!-- /.col-md-9 -->
-                            </div>
-                            <!-- /.row -->
                         </div>
-                        <!-- /.container -->';
-                    }elseif($_GET['page'] == 'produits')
-                    {
+                        <!-- /.col-md-9 -->';
+                    }elseif($_GET['page'] == 'article'){
+                        include('content.php');
                         echo '
-                        <div class="container">
-    
-                            <div class="row">';
-
-                                include('content.php');
-                                echo '
-                                <div class="col-md-9">';
-                                    include('productsNav.php');
-                                echo '
-                                </div>
-                                <!-- /.col-md-9 -->
-                            </div>
-                            <!-- /.row -->
+                        <div class="col-md-9">';
+                            include('article.php');
+                        echo '
                         </div>
-                        <!-- /.container -->';
-                    }elseif($_GET['page'] == 'aboutUs')
-                    {
+                        <!-- /.col-md-9 -->';
+                    }elseif($_GET['page'] == 'produits'){
+                        include('content.php');
+                        echo '
+                        <div class="col-md-9">';
+                            include('productsNav.php');
+                        echo '
+                        </div>
+                        <!-- /.col-md-9 -->';
+                    }elseif($_GET['page'] == 'aboutUs'){
                         include('aboutUs.php');
-                    }elseif($_GET['page'] == 'conditions')
-                    {
+                    }elseif($_GET['page'] == 'conditions'){
                         include('conditions.php');
                     }
 
-                    if(isConnected())
-                    {
-                        if($_GET['page'] == 'reservation')
-                        {
+                    if(isConnected()){
+                        if($_GET['page'] == 'reservation'){
                             include('reservation.php');
-                        }else if($_GET['page'] == 'profile')
-                        {
+                        }else if($_GET['page'] == 'profile'){
                             include('profile.php');
                         }
                     }
 
                     // ADMIN PAGES
 
-                    if(isAdmin())
-                    {
-                        if ($_GET['page'] == 'products')
-                        {
+                    if(isAdmin()){
+                        if ($_GET['page'] == 'products'){
                             include('admin/website.admin.php');
-                        } elseif ($_GET['page'] == 'articles')
-                        {
+                        }elseif ($_GET['page'] == 'articles'){
                             include('admin/articles.admin.php');
-                        } elseif ($_GET['page'] == 'users')
-                        {
+                        }elseif ($_GET['page'] == 'users'){
                             include('admin/users.admin.php');
-                        } elseif ($_GET['page'] == 'calendarAdmin')
-                        {
-                            echo '<div class="col-md-offset-3 col-md-9">';
+                        }elseif ($_GET['page'] == 'calendarAdmin'){
                             include('admin/calendar.admin.php');
                         }
                     }
 
                 }else{
+                    include('content.php');
                     echo '
-                        <div class="container">
-    
-                            <div class="row">';
-
-                                include('content.php');
-                                echo '
-                                <div class="col-md-9">';
-                                    include('home.php');
-                                echo '
-                                </div>
-                                <!-- /.col-md-9 -->
-                            </div>
-                            <!-- /.row -->
-                        </div>
-                        <!-- /.container -->';
+                    <div class="col-md-9">';
+                        include('home.php');
+                    echo '
+                    </div>
+                    <!-- /.col-md-9 -->';
                 }
 echo '
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->\';
 </body>
 ';
